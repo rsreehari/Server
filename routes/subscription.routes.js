2fb21313057249589a75cd0b95890ed24eace9e2
R SREEHARI <rsreehari091@gmail.com>
2025-08-29 17:24:00 +0530

    updated user route and subscription route

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,20 +2,23 @@ import { Router } from "express";
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/',(req ,res) => {
-    res.send({title : " GET all subscriptons"})
-});
+subscriptionRouter.get('/',(req ,res) => { res.send({title : " GET all subscripton"})});
 
-subscriptionRouter.get('/:id',(req ,res) => {
-    res.send({title : " GET user subscriptons"})
-});
-subscriptionRouter.post('/:id',(req ,res) => {
-    res.send({title : "ADD subscriptons"})
-});
+subscriptionRouter.get('/:id',(req ,res) => {res.send({title : " GET subscripton details"})});
 
-subscriptionRouter.delete('/:id',(req ,res) => {
-    res.send({title : " DELETE user subscriptons"})
-});
+subscriptionRouter.post('/',(req ,res) => {res.send({title : "CREATE subscripton"})});
+
+subscriptionRouter.put('/:id',(req ,res) => {res.send({title : "UPDATE subscriptons"})});
+
+subscriptionRouter.delete('/:id',(req ,res) => {res.send({title : " DELETE user subscriptons"})});
+
+subscriptionRouter.get('/user/:id',(req ,res) => {res.send({title : "GET all USER subscripton"})});
+
+subscriptionRouter.put('/:id/cancel',(req ,res) => {res.send({title : "CANCEL subscriptons"})});
+
+subscriptionRouter.get('/:id/upcoming-renewals',(req ,res) => {res.send({title : " DELETE user subscriptons"})});
+
+
 
 export default subscriptionRouter;
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { useActionState } from "react";
 
 const userRoute = Router();
 
