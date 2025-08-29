ffae939fa62a42975252f6fe8480418feccf7fa3
R SREEHARI <rsreehari091@gmail.com>
2025-08-29 17:40:43 +0530

    fix: correct auth route path and update sign-out response title

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 const authRouter = Router();
 
+authRouter.get("/",(req , res) => res.send({title : "auth route"}));
+
 authRouter.post('/sign-up',(req , res) => res.send({title : "sign-up"}));
 
 authRouter.post('/sign-in',(req , res) => res.send({title : "sign-in"}));
 
-authRouter.post('/sign-out',(req , res) => res.send({title : "sign-up"}));
+authRouter.post('/sign-out',(req , res) => res.send({title : "sign-out"}));
 
 export default authRouter;
